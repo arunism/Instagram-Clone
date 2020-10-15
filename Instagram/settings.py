971437a3ee81fb0e31454d8040abf83bfcd04ebe
hlwarun <hlwarun@gmail.com>
2020-10-15 17:58:54 +0545

    Notification Count

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
+                'notification.views.notification_counts',
             ],
         },
     },
--- notification/views.py
+++ notification/views.py
@@ -6,6 +6,15 @@ from notification.models import Notification
 def notification(request):
     user = request.user
     notifications = Notification.objects.filter(receiver=user).order_by('-created_at')
+    Notification.objects.filter(receiver=user, seen=False).update(seen=True)
 
     context = {'title': 'Notifications', 'notifications':notifications}
     return render(request, 'notification.html', context)
+
+def notification_counts(request):
+    user = request.user
+    count = 0
+    if user.is_authenticated:
+        count = Notification.objects.filter(receiver=user, seen=False).count()
+
+    return {'notification_count':count}
